76a047802e67249db51b7ca1ca360de9a8b7d7cb
ibayeye <169161293+ibayeye@users.noreply.github.com>
2024-06-06 17:01:39 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -2,13 +2,17 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 
 dotenv.config();
+// const db = new Sequelize("indodaya", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 
 const db = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 3306, // Gunakan DB_PORT atau default ke 3306
+  port: process.env.PORT,
   dialect: "mysql",
 });
 
