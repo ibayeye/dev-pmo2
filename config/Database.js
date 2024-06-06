3477057f859ca8c63830b079d3315ae71b8af94f
ibayeye <169161293+ibayeye@users.noreply.github.com>
2024-06-06 17:12:59 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -12,7 +12,7 @@ const db = new Sequelize({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   dialect: "mysql",
 });
 
